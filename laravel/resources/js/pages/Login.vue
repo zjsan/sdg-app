0034e3d3109a782a6f54393cf9dacb13397ce073
zjsan <zjsantos25@gmail.com>
2025-09-29 15:45:25 +0800

    added shadcdn components

--- laravel/resources/js/pages/Login.vue
+++ laravel/resources/js/pages/Login.vue
@@ -1,9 +1,49 @@
 <template>
     <div class="p-8 text-center bg-gray-50">
-        <h1 class="text-3xl font-bold">Welcome to SDG App 🚀</h1>
-        <router-link to="/login" class="text-blue-600 underline"
-            >Login</router-link
-        >
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>Login to your account</CardTitle>
+                <CardDescription>
+                    Enter your email below to login to your account
+                </CardDescription>
+                <CardAction>
+                    <Button variant="link">Sign Up</Button>
+                </CardAction>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <a
+                                    href="#"
+                                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                >
+                                    Forgot your password?
+                                </a>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full"> Login </Button>
+                <Button variant="outline" className="w-full">
+                    Login with Google
+                </Button>
+            </CardFooter>
+        </Card>
     </div>
 </template>
 <script setup>
@@ -16,4 +56,6 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 </script>
