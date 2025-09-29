d621c8295b07edb9795bd29111c69ad19ce7a021
zjsan <zjsantos25@gmail.com>
2025-09-29 11:07:05 +0800

    modifying default view of the url

--- laravel/resources/js/router/index.js
+++ laravel/resources/js/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../pages/Home.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+    {
+        path: "/",
+        redirect: "/login", // default redirect
+    },
+    {
+        path: "/login",
+        name: "Home",
+        component: Home,
+    },
+    // other routes...
+];
 
 export default createRouter({
     history: createWebHistory(),
