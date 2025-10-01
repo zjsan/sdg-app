9825d1af44c969b37dc301ed5e3b6c07ca1945aa
zjsan <zjsantos25@gmail.com>
2025-10-01 10:58:46 +0800

    added dashboard route in router/index.js

--- laravel/resources/js/pages/Dashboard.vue
+++ laravel/resources/js/pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <template>
     <h1>Dashboard</h1>
+    <p>Welcome to the dashboard!</p>
 </template>
 <script setup></script>
--- laravel/resources/js/router/index.js
+++ laravel/resources/js/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Login from "../pages/Login.vue";
+import Dashboard from "../pages/Dashboard.vue";
 import NotFoundView from "../pages/NotFoundView.vue";
 
 const routes = [
@@ -15,6 +16,14 @@ const routes = [
         name: "Login",
         component: Login,
     },
+
+    {
+        // Use path: "/login" without a trailing slash and with 'exact' behavior.
+        path: "/dashboard",
+        name: "Dashboard",
+        component: Dashboard,
+    },
+
     // Catch-all for 404 - MUST be the last route in the array.
     // or any other invalid path
     {
