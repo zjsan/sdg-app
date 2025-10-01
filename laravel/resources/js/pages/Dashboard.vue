f5ea4c9212ca7e1cffd75e6bb60989b4c5436e4a
zjsan <zjsantos25@gmail.com>
2025-10-01 14:15:14 +0800

    adding logout component in Dashboard.vue

--- laravel/resources/js/pages/Dashboard.vue
+++ laravel/resources/js/pages/Dashboard.vue
@@ -1,5 +1,25 @@
 <template>
     <h1>Dashboard</h1>
     <p>Welcome to the dashboard!</p>
+
+    <nav>
+        <Button
+            v-if="auth.isAuthenticated"
+            @click="logout"
+            type="submit"
+            class="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-700 transition"
+        >
+            Logout
+        </Button>
+        <router-link v-else to="/login">Login</router-link>
+    </nav>
 </template>
-<script setup></script>
+<script setup>
+import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/stores/auth";
+const auth = useAuthStore();
+
+const logout = async () => {
+    await auth.logout();
+};
+</script>
