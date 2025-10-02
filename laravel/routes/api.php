d7ccfd09f166a30378e55ee20626a606e353afa8
zjsan <zjsantos25@gmail.com>
2025-10-02 11:41:42 +0800

    added auth routes in api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//auth routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');  
