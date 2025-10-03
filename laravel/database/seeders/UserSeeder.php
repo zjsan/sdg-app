73db7e8478fe6e4e50307cb74741ba54e93dd74f
zjsan <zjsantos25@gmail.com>
2025-10-03 16:47:39 +0800

    created UserSeeder

---
+++ laravel/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::create([
+            'name' => 'Test User',
+            'username' => 'testuser',   
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'), 
+        ]);
+    }
+}
