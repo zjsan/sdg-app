65a5f0ee30617099e54f5b588bf80e8dce7016b6
zjsan <zjsantos25@gmail.com>
2025-10-04 17:44:59 +0800

    commented out session-regenerate()

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
             return response()->json(['message' => 'Invalid credentials.'], 401);
         }
 
-        $request->session()->regenerate(); // prevent session fixation
+        //$request->session()->regenerate(); // prevent session fixation
 
         return response()->json(['message' => 'Logged in successfully.']);
     }
