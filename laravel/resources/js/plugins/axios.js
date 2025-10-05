550f24662f5e957962542c4ccf45d9a408fd365c
zjsan <zjsantos25@gmail.com>
2025-10-05 12:59:43 +0800

    removed withCredentials in axios.js

--- laravel/resources/js/plugins/axios.js
+++ laravel/resources/js/plugins/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 // Create a reusable axios instance
 const api = axios.create({
     baseURL: "http://localhost:8080", // Laravel API base
-    withCredentials: true, // important for Sanctum cookies
     headers: {
         "X-Requested-With": "XMLHttpRequest",
     },
