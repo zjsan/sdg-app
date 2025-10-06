262ba3e1c10b0f07544df52aa72eeb49c9b8c0b5
zjsan <zjsantos25@gmail.com>
2025-10-06 14:52:39 +0800

    added shadcdn alert dialog

---
+++ laravel/resources/js/components/ui/alert-dialog/AlertDialogDescription.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactiveOmit } from "@vueuse/core";
+import { AlertDialogDescription } from "reka-ui";
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+  <AlertDialogDescription
+    v-bind="delegatedProps"
+    :class="cn('text-sm text-muted-foreground', props.class)"
+  >
+    <slot />
+  </AlertDialogDescription>
+</template>
