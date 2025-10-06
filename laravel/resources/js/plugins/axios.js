39bacf2aec3c97d48ae71829f04a8b6b32124a7e
zjsan <zjsantos25@gmail.com>
2025-10-06 08:30:56 +0800

    modified basedURL for laravel api in axios.js

--- laravel/resources/js/plugins/axios.js
+++ laravel/resources/js/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a reusable axios instance
 const api = axios.create({
-    baseURL: "http://localhost:8080", // Laravel API base
+    baseURL: "http://localhost:8080/api", // Laravel API base
     headers: {
         "X-Requested-With": "XMLHttpRequest",
     },
