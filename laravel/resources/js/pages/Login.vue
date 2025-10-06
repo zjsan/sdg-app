01a2a85a78d4cf538164a5587bc0bb63d598df26
zjsan <zjsantos25@gmail.com>
2025-10-06 10:16:51 +0800

    updating Login.vue and adding new image in images directory

--- laravel/resources/js/pages/Login.vue
+++ laravel/resources/js/pages/Login.vue
@@ -1,9 +1,9 @@
 <template>
     <div
-        class="min-h-screen flex items-center justify-center bg-gray-100 px-4 text-gray-900"
+        class="min-h-screen flex items-center justify-center bg-white px-4 text-gray-900"
     >
         <div
-            class="flex flex-col items-center justify-center gap-5 sm:flex-row w-full max-w-4xl"
+            class="flex flex-col items-center justify-center gap-8 sm:flex-row w-full max-w-4xl"
         >
             <div class="max-w-sm sm:max-w-sm md:max-w-md">
                 <img
