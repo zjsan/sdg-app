cd7c03d1cd06a68e7dec582cc42e417f4cb5eab3
zjsan <zjsantos25@gmail.com>
2025-10-06 11:04:39 +0800

    mounted primeicons in app.js

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import router from "./router";
 import "./bootstrap";
 import "../css/app.css";
+import "primeicons/primeicons.css"; // Import the PrimeIcons CSS
 
 import App from "./pages/App.vue";
 
