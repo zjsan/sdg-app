e1d6d61a6e98c4e5116a80cd4edefbacb7068f8d
zjsan <zjsantos25@gmail.com>
2025-10-07 09:05:21 +0800

    replaced the powerbi link in powerbi controller

--- laravel/app/Http/Controllers/PowerBiController.php
+++ laravel/app/Http/Controllers/PowerBiController.php
@@ -13,7 +13,7 @@ class PowerBiController extends Controller
     public function getEmbedUrl(Request $request)
     {
         //  Only authenticated users reach this point due to Sanctum middleware
-        $powerBiUrl = 'https://app.powerbi.com/view?r=eyJrIjoiZmRiODdmMzgtZTk2ZS00MTQ1LTgyY2YtZmEyYmU2N2RkYTA5IiwidCI6IjdjZmY5YzA2LThmNGQtNDAwNi1iOWQwLWU4MWRjYWJjZDU1NyIsImMiOjEwfQ%3D%3D';
+        $powerBiUrl = "https://app.powerbi.com/view?r=eyJrIjoiZmRiODdmMzgtZTk2ZS00MTQ1LTgyY2YtZmEyYmU2N2RkYTA5IiwidCI6IjdjZmY5YzA2LThmNGQtNDAwNi1iOWQwLWU4MWRjYWJjZDU1NyIsImMiOjEwfQ%3D%3D";
 
         return response()->json([
             'url' => $powerBiUrl,
