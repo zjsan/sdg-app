23f18068f8288971e3ecafabf3873b7111d747d5
zjsan <zjsantos25@gmail.com>
2025-10-07 09:01:39 +0800

    created powerbi controller logic

--- laravel/app/Http/Controllers/PowerBiController.php
+++ laravel/app/Http/Controllers/PowerBiController.php
@@ -7,4 +7,16 @@ use Illuminate\Http\Request;
 class PowerBiController extends Controller
 {
     //
+      /**
+     * Return Power BI embed URL for authenticated users.
+     */
+    public function getEmbedUrl(Request $request)
+    {
+        //  Only authenticated users reach this point due to Sanctum middleware
+        $powerBiUrl = 'https://app.powerbi.com/view?r=eyJrIjoiZmRiODdmMzgtZTk2ZS00MTQ1LTgyY2YtZmEyYmU2N2RkYTA5IiwidCI6IjdjZmY5YzA2LThmNGQtNDAwNi1iOWQwLWU4MWRjYWJjZDU1NyIsImMiOjEwfQ%3D%3D';
+
+        return response()->json([
+            'url' => $powerBiUrl,
+        ]);
+    }
 }
