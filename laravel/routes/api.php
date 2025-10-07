f132e0e8bfb65623ecd2c06a96967e557e45eeb4
zjsan <zjsantos25@gmail.com>
2025-10-07 09:03:51 +0800

    added the controller in the routes/api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\PowerBiController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -11,3 +11,6 @@ Route::get('/user', function (Request $request) {
 //auth routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');  
+
+// Protected route to get Power BI embed URL
+Route::middleware('auth:sanctum')->get('/powerbi-url', [PowerBiController::class, 'getEmbedUrl']);
