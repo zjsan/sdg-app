e8ae2d7fea573b8e6b415cd221e6d8468772a3ec
zjsan <zjsantos25@gmail.com>
2025-10-20 11:43:24 +0800

    modifying App.vue

--- laravel/resources/js/pages/App.vue
+++ laravel/resources/js/pages/App.vue
@@ -5,11 +5,15 @@
 <script setup>
 import { onMounted } from "vue";
 import { useAuthStore } from "../stores/auth";
+import { useRoute } from "vue-router";
 
 const auth = useAuthStore();
+const route = useRoute();
 
 onMounted(() => {
     // Try to restore session
-    auth.getUser();
+    if (auth.token && route.meta.requiresAuth) {
+        auth.getUser();
+    }
 });
 </script>
