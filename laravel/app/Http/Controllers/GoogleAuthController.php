fdf9e037ae4eaf4c61b9140645744fa59c2d54b8
zjsan <zjsantos25@gmail.com>
2025-10-29 10:27:04 +0800

    added controller logic for google auth redirect

--- laravel/app/Http/Controllers/GoogleAuthController.php
+++ laravel/app/Http/Controllers/GoogleAuthController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class GoogleAuthController extends Controller
 {
-    //
+   /**
+     * Redirect the user to Google for authentication.
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+
 }
