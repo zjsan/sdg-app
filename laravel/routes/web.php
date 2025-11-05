80ab668e3d15e784558243ae7b2821e69b3fd51c
zjsan <zjsantos25@gmail.com>
2025-11-05 10:12:43 +0800

    added new route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\GoogleAuthController;
 //Google OAuth Routes
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
+Route::get('/auth/session/{sessionId}', [GoogleAuthController::class, 'fetchSessionData']);
 
 Route::get('/{any}', function () {
     return view('welcome'); 
