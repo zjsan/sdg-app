d25afac61a45df29c4b0bd3c7ac77b51702c173c
zjsan <zjsantos25@gmail.com>
2025-11-06 08:41:27 +0800

    updated comment

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     <title>Laravel</title>
 </head>
 <body>
-    {{-- This is the element Vue needs to attach to --}}
+    {{--  element Vue needs to attach to --}}
     <div id="app"></div>
 
-    {{-- Make sure your Vite directive is here to load the JS --}}
+    {{--  Vite directive is here to load the JS --}}
     @vite('resources/js/app.js')
 </body>
 </html> 
