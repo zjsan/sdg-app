db929acb9af5337956e01c6cefa9734d9e7a16cb
zjsan <zjsantos25@gmail.com>
2025-11-07 12:09:17 +0800

    added condition in returning power bi url

--- laravel/app/Http/Controllers/PowerBiController.php
+++ laravel/app/Http/Controllers/PowerBiController.php
@@ -21,6 +21,9 @@ class PowerBiController extends Controller
             //fetch email from users table
             $email = DB::table('users')->select('email')->get();
             
+            //Check if email domain is in the whitelist
+            //If yes, return the Power BI embed URL for mmsu users
+            //Else return the Power BI embed URL for external users
             if(Str::contains($email, $domain_whitelist)){
                 //  Only authenticated users reach this point due to Sanctum middleware
                 $powerBiUrl = "https://app.powerbi.com/view?r=eyJrIjoiZmRiODdmMzgtZTk2ZS00MTQ1LTgyY2YtZmEyYmU2N2RkYTA5IiwidCI6IjdjZmY5YzA2LThmNGQtNDAwNi1iOWQwLWU4MWRjYWJjZDU1NyIsImMiOjEwfQ%3D%3D";
@@ -29,10 +32,17 @@ class PowerBiController extends Controller
                     'url' => $powerBiUrl,
                 ]);
             }
+            else{
+                //https://app.powerbi.com/view?r=eyJrIjoiNzZhNzAxMTQtYmQ4YS00ZTFiLTllMTgtNzgwMzQyMWFkOTc2IiwidCI6IjdjZmY5YzA2LThmNGQtNDAwNi1iOWQwLWU4MWRjYWJjZDU1NyIsImMiOjEwfQ%3D%3D
+                 $powerBiUrl = "https://app.powerbi.com/view?r=eyJrIjoiNzZhNzAxMTQtYmQ4YS00ZTFiLTllMTgtNzgwMzQyMWFkOTc2IiwidCI6IjdjZmY5YzA2LThmNGQtNDAwNi1iOWQwLWU4MWRjYWJjZDU1NyIsImMiOjEwfQ%3D%3D";
+                return response()->json([
+                    'url' => $powerBiUrl, 
+                ]);
+            }
 
         } catch (\Throwable $e) {
             return response()->json([
-                'message' => 'Unauthorized access.'
+                'message' => 'Fetching dashboard url error.'
             ], 401);
         }
 
