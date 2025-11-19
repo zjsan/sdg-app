babfc9ba6683d986f876a069e92cc9e9ab4d1fb5
zjsan <zjsantos25@gmail.com>
2025-11-19 13:44:32 +0800

    updated onMounted function

--- laravel/resources/js/pages/SafeRedirect.vue
+++ laravel/resources/js/pages/SafeRedirect.vue
@@ -7,12 +7,18 @@
 <script setup>
 import { onMounted } from "vue";
 import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
 
 const router = useRouter();
+const auth = useAuthStore();
 
 onMounted(() => {
     // runs after the callback component completes its job.
     // It's the final, clean redirect.
-    router.replace({ name: "Dashboard" });
+    // ensure the user is logged in before redirecting
+    if (auth.isAuthenticated) {
+        //This replaces the /safe-redirect entry with the clean /dashboard entry.
+        router.replace({ name: "Dashboard" });
+    }
 });
 </script>
