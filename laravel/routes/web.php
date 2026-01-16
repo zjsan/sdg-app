c1e999eb0d38036486d1261f86d47c23bcef85ab
zjsan <zjsantos25@gmail.com>
2026-01-16 08:26:45 +0800

    fixed typo in powerbi controller import

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleAuthController;
-use App\Http\Controllers\PowerBIController;
+use App\Http\Controllers\PowerBiController;
 
 //Google OAuth Routes
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
 
 //handle iframe request for Power BI link
-Route::get('/pbi-frame', [PowerBIController::class, 'serveIframe'])
+Route::get('/pbi-frame', [PowerBiController::class, 'serveIframe'])
     ->name('pbi.frame');
 
 Route::get('/{any}', function () {
