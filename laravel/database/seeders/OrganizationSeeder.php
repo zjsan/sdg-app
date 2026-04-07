28aff5e8dcfd53e6c3d26328b18c07b2a961d3a4
zjsan <zjsantos25@gmail.com>
2026-04-07 11:58:11 +0800

    added comments

--- laravel/database/seeders/OrganizationSeeder.php
+++ laravel/database/seeders/OrganizationSeeder.php
@@ -12,7 +12,7 @@ class OrganizationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //initial database seeding for the organization table
         \App\Models\Organization::updateOrCreate(
             ['slug' => 'mmsu'],
             ['name' => 'MMSU', 'pbi_embed_id' => config('app.power_bi.MMSU_EMBED_ID')] // MMSU ID
