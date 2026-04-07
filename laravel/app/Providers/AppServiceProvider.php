4f50a36b57f6fb98b691ae3e948ce3dd32f52a41
zjsan <zjsantos25@gmail.com>
2026-04-07 10:41:26 +0800

    creating gates for secured athorization

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Organization;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
     }
 }
