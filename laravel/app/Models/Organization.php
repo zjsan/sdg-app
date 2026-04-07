6b770e1cdff0c99a589e75c29fe3935e1dfc1fb0
zjsan <zjsantos25@gmail.com>
2026-04-07 08:42:58 +0800

    inserted contents in organization table

--- laravel/app/Models/Organization.php
+++ laravel/app/Models/Organization.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Organization extends Model
 {
     //
+    protected $fillable = ['name', 'slug', 'pbi_embed_id'];//whitelist these fields for mass assignment
+
+    public function users() {
+        return $this->hasMany(User::class);
+    }
 }
