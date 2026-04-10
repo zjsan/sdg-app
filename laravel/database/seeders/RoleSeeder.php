543168d18d63c42c5a8922626ae59eb6cd19b3ed
zjsan <zjsantos25@gmail.com>
2026-04-10 14:46:25 +0800

    created role seeder

--- laravel/database/seeders/RoleSeeder.php
+++ laravel/database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -13,5 +14,14 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         //
+        $roles = [
+        ['name' => 'Developer', 'slug' => 'developer'],
+        ['name' => 'Admin', 'slug' => 'admin'],
+        ['name' => 'Viewer', 'slug' => 'viewer'],
+    ];
+
+    foreach ($roles as $role) {
+        Role::updateOrCreate(['slug' => $role['slug']], $role);
+        }
     }
 }
