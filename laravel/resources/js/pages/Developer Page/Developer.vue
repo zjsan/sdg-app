7143c2cbdc0c0ed555d80184f2f682ec7e7d1393
zjsan <zjsantos25@gmail.com>
2026-04-13 15:48:24 +0800

    updating developer page

--- laravel/resources/js/pages/Developer Page/Developer.vue
+++ laravel/resources/js/pages/Developer Page/Developer.vue
@@ -1,10 +1,13 @@
 <template>
-    <h1>Developer Page</h1>
-    <button @click="logout">Logout</button>
+    <Authenticated>
+        <div class="p-8">
+            <h1 class="text-2xl font-bold">Admin Control Panel</h1>
+        </div>
+    </Authenticated>
 </template>
 <script setup>
 import { useAuthStore } from "@/stores/auth";
-
+import Authenticated from "../Dashboard Template/Layout/Authenticated.vue";
 const auth = useAuthStore();
 
 const logout = async () => {
