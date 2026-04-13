cca92b0a5f416214af808528e2bf900a634e6a27
zjsan <zjsantos25@gmail.com>
2026-04-13 15:22:38 +0800

    updated sample developer page

--- laravel/resources/js/pages/Developer Page/Developer.vue
+++ laravel/resources/js/pages/Developer Page/Developer.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>Developer Page</h1>
+    <button @click="logout">Logout</button>
 </template>
+<script setup>
+import { useAuthStore } from "@/stores/auth";
+
+const auth = useAuthStore();
+
+const logout = async () => {
+    await auth.logout();
+};
+</script>
