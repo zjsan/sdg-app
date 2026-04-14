e9bd2971f994c1a4b22318f91e3ec8f1efa0f747
zjsan <zjsantos25@gmail.com>
2026-04-14 13:43:18 +0800

    changed back to production

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://app.sdg-dashboard.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -123,7 +123,7 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'frontend_url' => env('FRONTEND_URL', 'http://localhost:8080'),
+    'frontend_url' => env('FRONTEND_URL', 'https://app.sdg-dashboard.com'),
 
     'power_bi' => [
         'BASE_URL' => env('POWER_BI_BASE_URL', 'https://app.powerbi.com/view?r='),
