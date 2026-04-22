5511a73f00fe45bb2e104a3eb190e4094c2a5402
zjsan <zjsantos25@gmail.com>
2026-04-22 08:50:53 +0800

    updated logout button color

--- laravel/resources/js/pages/Dashboard Template/Component/UserSection.vue
+++ laravel/resources/js/pages/Dashboard Template/Component/UserSection.vue
@@ -19,7 +19,7 @@
         <AlertDialog v-if="auth.isAuthenticated">
             <AlertDialogTrigger as-child>
                 <Button
-                    class="w-full justify-center bg-gray-800 hover:bg-gray-600 text-white"
+                    class="w-full justify-center bg-blue-400 hover:bg-blue-600 text-white"
                 >
                     <i class="pi pi-sign-out mr-2"></i> Logout
                 </Button>
@@ -36,7 +36,7 @@
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
                     <AlertDialogAction
                         @click="logout"
-                        class="bg-gray-800 hover:bg-gray-400"
+                        class="bg-blue-400 hover:bg-blue-600"
                     >
                         Yes, Log me out
                     </AlertDialogAction>
