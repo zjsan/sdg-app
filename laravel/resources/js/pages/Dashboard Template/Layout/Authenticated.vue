a24c6d27860ebfbc96dbfb6371f39c087356f1a6
zjsan <zjsantos25@gmail.com>
2026-04-23 14:10:57 +0800

    fixing main layout styles

--- laravel/resources/js/pages/Dashboard Template/Layout/Authenticated.vue
+++ laravel/resources/js/pages/Dashboard Template/Layout/Authenticated.vue
@@ -1,40 +1,41 @@
 <template>
     <div class="flex h-screen bg-gray-50 overflow-hidden">
-        <header
-            class="h-14 lg:hidden border-b bg-white flex items-center justify-between p-4 shadow-sm flex-shrink-0"
-        >
-            <Button
-                @click="isSidebarOpen = !isSidebarOpen"
-                variant="ghost"
-                size="icon"
-            >
-                <i
-                    :class="isSidebarOpen ? 'pi pi-times' : 'pi pi-bars'"
-                    class="text-xl"
-                ></i>
-            </Button>
-        </header>
-
         <!-- SIDEBAR -->
         <Sidebar
             :is-open="isSidebarOpen"
             :is-mobile="isMobile"
             @close="isSidebarOpen = false"
         />
-
         <div
             v-if="isSidebarOpen && isMobile"
             @click="isSidebarOpen = false"
             class="fixed inset-0 z-30 bg-black/50 lg:hidden"
         ></div>
+        <!--Right side-->
+        <div class="flex flex-col flex-1 min-w-0">
+            <header
+                class="h-14 lg:hidden border-b bg-white flex items-center justify-between p-4 shadow-sm flex-shrink-0"
+            >
+                <Button
+                    @click="isSidebarOpen = !isSidebarOpen"
+                    variant="ghost"
+                    size="icon"
+                >
+                    <i
+                        :class="isSidebarOpen ? 'pi pi-times' : 'pi pi-bars'"
+                        class="text-xl"
+                    ></i>
+                </Button>
+            </header>
 
-        <!-- SCROLLABLE CONTENT AREA -->
-        <div class="flex-1 overflow-y-auto">
-            <main class="p-6">
-                <slot />
-            </main>
+            <!-- SCROLLABLE CONTENT AREA -->
+            <div class="flex-1 overflow-y-auto">
+                <main class="p-6">
+                    <slot />
+                </main>
 
-            <Footer />
+                <Footer />
+            </div>
         </div>
     </div>
 </template>
