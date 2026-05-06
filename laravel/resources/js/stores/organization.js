3fb6f2930cffe769842caa7e64c9489a6ce2c4d4
zjsan <zjsantos25@gmail.com>
2026-05-06 09:37:27 +0800

    created createOrganization api call and added state values

--- laravel/resources/js/stores/organization.js
+++ laravel/resources/js/stores/organization.js
@@ -28,7 +28,27 @@ export const useOrganizationStore = defineStore("organization", {
             }
         },
 
+        async createOrganization(name, pbi_embed_id) {
+            this.loading = true;
+            try {
+                const response = await api.post("/organizations", {
+                    name,
+                    pbi_embed_id,
+                });
+                this.organizations.push(response.data); //update the local state with the new organization
+                return response.message || "Organization created successfully.";
+            } catch (error) {
+                this.errors =
+                    error.response?.data?.message ||
+                    "Failed to create organization.";
+                console.error("Failed to create organization:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+
         async UpdateOrganizations(id, newId) {
+            this.loading = true;
             try {
                 await api.put(`/organizations/${id}`, {
                     pbi_embed_id: newId,
@@ -37,7 +57,12 @@ export const useOrganizationStore = defineStore("organization", {
                     `Organization ${id} updated with new PBI Embed ID: ${newId}`,
                 );
             } catch (error) {
+                this.errors =
+                    error.response?.data?.message ||
+                    `Failed to update organization ${id}.`;
                 console.error(error);
+            } finally {
+                this.loading = false;
             }
         },
     },
