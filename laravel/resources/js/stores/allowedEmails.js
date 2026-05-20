2ceb5843b5ffd6a4b678d6780a05827047e98b1a
zjsan <zjsantos25@gmail.com>
2026-05-20 13:56:08 +0800

    created pinia action update function

--- laravel/resources/js/stores/allowedEmails.js
+++ laravel/resources/js/stores/allowedEmails.js
@@ -30,16 +30,38 @@ export const useAllowedEmailsStore = defineStore("allowedEmails", {
         async addAllowedEmails(email) {
             this.loading = true;
             try {
-                const response = await api.post("/allowed-emails", {
+                const res = await api.post("/allowed-emails", {
                     email,
                 });
-                this.emails.unshift(response.data); //update the local state with the new allowed email
-                return response.message || "Allowed email added successfully.";
+                this.emails.unshift(res.data); //update the local state with the new allowed email
+                return res.message || "Allowed email added successfully.";
             } catch (error) {
                 this.errors =
                     error.response?.data?.message ||
                     "Failed to add allowed email.";
                 console.error("Failed to add allowed email:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+
+        async updateAllowedEmails(id, newEmail) {
+            this.loading = true;
+
+            try {
+                const res = await api.put(`/allowed-emails/${id}`, newEmail);
+
+                const index = this.emails.findIndex((email) => email.id === id);
+                if (index !== -1) {
+                    this.emails[index] = res.data; // Update the local state with the updated email
+                }
+
+                return res.message || "Allowed email updated successfully.";
+            } catch (error) {
+                this.errors =
+                    error.response?.data?.message ||
+                    "Failed to update allowed email.";
+                console.error("Failed to update allowed email:", error);
             } finally {
                 this.loading = false;
             }
