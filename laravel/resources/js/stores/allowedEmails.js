0d2c88cb600a5ffd8b525a0bc7a83338f3bc5bd0
zjsan <zjsantos25@gmail.com>
2026-05-20 13:50:34 +0800

    created fetching and adding new emails pinia actions

--- laravel/resources/js/stores/allowedEmails.js
+++ laravel/resources/js/stores/allowedEmails.js
@@ -7,5 +7,42 @@ export const useAllowedEmailsStore = defineStore("allowedEmails", {
         loading: false,
         errors: null,
     }),
-    actions: {},
+    actions: {
+        async fetchAllowedEmails() {
+            this.loading = true;
+
+            try {
+                const res = await api.get("/allowed-emails");
+
+                this.organizations = Array.isArray(res.data)
+                    ? res.data
+                    : res.data.data;
+            } catch (error) {
+                this.errors =
+                    error.response?.data?.message ||
+                    "Failed to load allowed emails.";
+                console.error("Failed to fetch allowed emails:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+
+        async addAllowedEmails(email) {
+            this.loading = true;
+            try {
+                const response = await api.post("/allowed-emails", {
+                    email,
+                });
+                this.emails.unshift(response.data); //update the local state with the new allowed email
+                return response.message || "Allowed email added successfully.";
+            } catch (error) {
+                this.errors =
+                    error.response?.data?.message ||
+                    "Failed to add allowed email.";
+                console.error("Failed to add allowed email:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+    },
 });
