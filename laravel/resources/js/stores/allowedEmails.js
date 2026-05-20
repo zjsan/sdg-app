1838e6e295cd61cf1c52c0a7096c577c90d061f6
zjsan <zjsantos25@gmail.com>
2026-05-20 14:01:37 +0800

    created pinia action for delete function

--- laravel/resources/js/stores/allowedEmails.js
+++ laravel/resources/js/stores/allowedEmails.js
@@ -17,6 +17,7 @@ export const useAllowedEmailsStore = defineStore("allowedEmails", {
                 this.organizations = Array.isArray(res.data)
                     ? res.data
                     : res.data.data;
+                console.log("Allowed Emails fetched:", this.emails); // Debugging log to check the fetched data
             } catch (error) {
                 this.errors =
                     error.response?.data?.message ||
@@ -34,7 +35,7 @@ export const useAllowedEmailsStore = defineStore("allowedEmails", {
                     email,
                 });
                 this.emails.unshift(res.data); //update the local state with the new allowed email
-                return res.message || "Allowed email added successfully.";
+                console.log(res.message || "Allowed email added successfully.");
             } catch (error) {
                 this.errors =
                     error.response?.data?.message ||
@@ -56,12 +57,38 @@ export const useAllowedEmailsStore = defineStore("allowedEmails", {
                     this.emails[index] = res.data; // Update the local state with the updated email
                 }
 
-                return res.message || "Allowed email updated successfully.";
+                console.log(
+                    res.message || "Allowed email updated successfully.",
+                );
             } catch (error) {
                 this.errors =
                     error.response?.data?.message ||
                     "Failed to update allowed email.";
                 console.error("Failed to update allowed email:", error);
+            } finally {
+                this.loading = false;
+            }
+        },
+
+        async deleteAllowedEmails(id) {
+            this.loading = true;
+
+            if (!confirm("Are you sure?")) {
+                return;
+            }
+
+            try {
+                await api.delete(`/allowed-emails/${id}`);
+
+                this.emails = this.emails.filter((email) => email.id !== id); // Update local state by removing the deleted email
+                console.log(
+                    res.message || "Allowed email deleted successfully.",
+                );
+            } catch (error) {
+                this.errors =
+                    error.response?.data?.message ||
+                    "Failed to delete allowed email.";
+                console.error("Failed to delete allowed email:", error);
             } finally {
                 this.loading = false;
             }
