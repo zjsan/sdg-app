5b716d6608601059547d020f30feabe665825199
zjsan <zjsantos25@gmail.com>
2026-05-22 09:42:20 +0800

    updated model class name and added casting for the is_active field

--- laravel/app/Models/AllowedEmail.php
+++ laravel/app/Models/AllowedEmail.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AllowedEmails extends Model
+class AllowedEmail extends Model
 {
     //
     protected $fillable = [
@@ -12,6 +12,11 @@ class AllowedEmails extends Model
         'is_active',
         'organization_id',
         'role_id',
+    ];
+
+    //casting the is_active field to boolean for easier handling in the application
+    protected $casts = [
+        'is_active' => 'boolean',
     ];
 
     public function role()
