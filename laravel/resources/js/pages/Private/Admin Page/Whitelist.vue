6d1fa2b979a4cc21ac002131ae5e8dd5faeaa0e3
zjsan <zjsantos25@gmail.com>
2026-05-22 13:48:32 +0800

    added new component imports in Whitelist.vue

--- laravel/resources/js/pages/Private/Admin Page/Whitelist.vue
+++ laravel/resources/js/pages/Private/Admin Page/Whitelist.vue
@@ -4,12 +4,19 @@
             <PageHeader>
                 <template #title>Whitelist Management</template>
             </PageHeader>
+
+            <div
+                class="flex justify-end py-4 border-b border-slate-100 flex justify-end bg-slate-50/50"
+            ></div>
         </div>
     </Authenticated>
 </template>
 <script setup>
 import Authenticated from "../Dashboard Template/Layout/Authenticated.vue";
 import PageHeader from "../Dashboard Template/Component/PageHeader.vue";
+import AppTable from "../Dashboard Template/Component/AppTable.vue";
+import BaseButton from "../Dashboard Template/Component/BaseButton.vue";
+import BaseModal from "../Dashboard Template/Component/BaseModal.vue";
 import { useAllowedEmailsStore } from "@/stores/allowedEmails";
 
 const allowedEmailsStore = useAllowedEmailsStore();
