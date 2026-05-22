9e6fa105d657cd5476c5aa99f5e7cc86998d43af
zjsan <zjsantos25@gmail.com>
2026-05-22 11:36:45 +0800

    updating whitelist component template

--- laravel/resources/js/pages/Private/Admin Page/Whitelist.vue
+++ laravel/resources/js/pages/Private/Admin Page/Whitelist.vue
@@ -1,20 +1,12 @@
 <template>
-    <h1>Admin Page</h1>
-    <div
-        class="flex flex-col sm:flex-row sm:items-end justify-between gap-4 mb-8"
-    >
-        <div>
-            <h1 class="text-3xl font-extrabold text-slate-900 tracking-tight">
-                Allowed Emails Management
-            </h1>
-            <!-- <p class="mt-1 text-sm text-slate-500">
-                <span class="font-medium text-slate-700">{{
-                    organizationStore.organizations.length
-                }}</span>
-                active organizations.
-            </p> -->
+    <Authenticated>
+        <div class="p-4">
+            <h1 class="text-2xl font-bold mb-4">Whitelist Management</h1>
+            <p class="mb-2">
+                This page is under construction. Please check back later.
+            </p>
         </div>
-    </div>
+    </Authenticated>
 </template>
 <script setup>
 import Authenticated from "../Dashboard Template/Layout/Authenticated.vue";
