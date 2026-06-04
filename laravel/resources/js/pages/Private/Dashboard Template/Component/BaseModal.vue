cffde30a6648b132b7a3e65695f3e3818e816b31
zjsan <zjsantos25@gmail.com>
2026-06-04 11:50:45 +0800

    removed div container for displaying debugging state of the ui modal

--- laravel/resources/js/pages/Private/Dashboard Template/Component/BaseModal.vue
+++ laravel/resources/js/pages/Private/Dashboard Template/Component/BaseModal.vue
@@ -1,9 +1,5 @@
 <template>
     <Teleport to="body">
-        <div class="fixed top-0 left-0 bg-red-500 text-white p-2 z-[9999]">
-            BaseModal Loaded. Prop 'show' value is: {{ show }}
-        </div>
-
         <div
             v-if="show"
             class="fixed inset-0 z-50 flex items-center justify-center p-4"
