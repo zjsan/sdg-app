e269215ea9f15ed4cd83473db242d2f122864a76
zjsan <zjsantos25@gmail.com>
2026-06-15 09:40:35 +0800

    added use HasFactory; in the organization model

--- laravel/app/Models/Organization.php
+++ laravel/app/Models/Organization.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Organization extends Model
 {
+
+    use HasFactory;
+    
     //
     protected $fillable = ['name', 'slug', 'pbi_embed_id'];//whitelist these fields for mass assignment
 
