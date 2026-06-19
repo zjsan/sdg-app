dbf8bea39e76cb475e95c4ef1062181c33ec5626
zjsan <zjsantos25@gmail.com>
2026-06-19 15:45:17 +0800

    updated allowed emails table to use also the soft delete architecture

---
+++ laravel/database/migrations/2026_06_19_074426_add_soft_deletes_to_allowed_emails_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('allowed_emails', function (Blueprint $table) {
+            //
+            $table->softDeletes(); // Adds 'deleted_at' column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('allowed_emails', function (Blueprint $table) {
+            //
+            $table->dropSoftDeletes();
+        });
+    }
+};
