84fde619e661618c7546ad30388dd917d68ad567
zjsan <zjsantos25@gmail.com>
2026-06-19 13:45:48 +0800

    added soft delete trait

--- laravel/app/Models/Organization.php
+++ laravel/app/Models/Organization.php
@@ -5,18 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes; 
 
 class Organization extends Model
 {
 
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     
     //
     protected $fillable = ['name', 'slug', 'pbi_embed_id'];//whitelist these fields for mass assignment
 
     // Dates array ensures deleted_at is cast to a Carbon instance automatically
     protected $dates = ['deleted_at'];
-    
+
     public function users() {
         return $this->hasMany(User::class);
     }
